a034c4ca19ca076f0ba10f623d7ce0b96ad0d661
thatisbummer <tjdals4358@naver.com>
2025-04-14 17:45:24 +0900

    cache clear

--- backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/blog/controller/BlogController.java
+++ backend/src/main/java/com/fiveguys/fivelogbackend/domain/blog/blog/controller/BlogController.java
@@ -40,7 +40,7 @@ public class BlogController {
 
     @Operation(summary = "사용자 목록 조회", description = "사용자 목록을 페이지 단위로 조회합니다.") // 스웨거
     @GetMapping("/users")
-    public ResponseEntity<Page<Board>> getBoards(@PageableDefault(page = 1, size = 10) Pageable pageable) {
+    public ResponseEntity<Page<Board>> getBoards(@PageableDefault(page = 0, size = 10) Pageable pageable) {
         Page<Board> boards = boardService.getBoards(pageable);
         return ResponseEntity.ok(boards);
     }
@@ -50,7 +50,7 @@ public class BlogController {
     @GetMapping("search")
     public ResponseEntity<Page<Board>> searchBoards(
             @RequestParam String searchContent, // 검색 내용
-            @PageableDefault(size = 10, page = 1) Pageable pageable) {
+            @PageableDefault(size = 10, page = 0) Pageable pageable) {
         Page<Board> searchResult = blogService.searchBoards(searchContent, pageable);
         return ResponseEntity.ok(searchResult);
     }
